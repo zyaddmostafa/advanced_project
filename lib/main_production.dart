d1f9b74a10f75ea256db8a2f86f4e05df63b65c7
zyad mostafa <zyadmo026@gmail.com>
2025-03-01 15:48:09 +0200

    - added development and production  flavor for Android - added launch.json for flavor config - change app name for each flavor - added tow main files for each flavor

---
+++ lib/main_production.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_advanced/core/di/dependency_injection.dart';
+import 'package:flutter_advanced/core/routing/app_router.dart';
+import 'package:flutter_advanced/doc_app.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() async {
+  // Ensure the screen size is set before the app starts in release mode
+  await ScreenUtil.ensureScreenSize();
+  setupGetIt();
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
+}
